30e61054dbccb397687ecfb6676aebfa5bb58fea
CedricLahaye <cedric.lahaye@my-digital-school.org>
2022-11-25 08:47:00 +0100

    Project init + Products CRUD

---
+++ src/Entity/Products.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductsRepository::class)]
+class Products
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $stocks = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStocks(): ?int
+    {
+        return $this->stocks;
+    }
+
+    public function setStocks(int $stocks): self
+    {
+        $this->stocks = $stocks;
+
+        return $this;
+    }
+}
